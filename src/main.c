5bf532c64bb4f62f94842c14ab4bdba50789d23d
sharma-a <sharma.a1987@gmail.com>
2022-09-27 17:56:06 +0530

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,24 +1,27 @@
 #include <stdio.h>
 #include <uncertain.h>
+#include <math.h>
+
 
 int
 main(int argc, char *  argv[])
 {
-	double	a, b, c;
+    double  a, b, c, d;
+    double ss=5.0;
+    a=libUncertainDoubleExponentialDist(3.0);
+    b=libUncertainDoubleGaussDist(10.0,5.0);
+    c=(a*b/sqrt(b*b+a));
+    d=a*a+b*b;
+    printf("Mean value of c is: %lf\n",c);
+    printf("Mean value of d is: %lf\n",d);
+    printf("Variance of b (which is %lf actually) is: %lf\n",ss*ss,libUncertainDoubleNthMoment(b,2)); 
 
-	a = libUncertainDoubleUniformDist(0.5, 1.0);
-	printf("a = %lf\n", a);
+    printf("Variance of c is: %lf\n",libUncertainDoubleNthMoment(c,2));
+    printf("Variance of d is: %lf\n",libUncertainDoubleNthMoment(d,2));
 
-	b = libUncertainDoubleUniformDist(10.0, 20.0);
-	printf("b = %lf\n", b);
 
-	c = (a+b)/(a-b);
-	printf("c = %lf\n", c);
 
-#ifdef DEBUG
-	printf("debug message\n");
-#endif
-
-	return 0;
+    return 0;
 }
+
 
